7d429e5587752d45234645bfb0f347a8eb55a328
Norhan-127 <norhanhamza127@gmail.com>
2023-12-12 13:08:31 +0200

    create styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -9,19 +10,83 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.04),
+      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.04),
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
           FeaturedBooksListView(),
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 50,
+          ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            Text('Best Seller',style: Styles.titleMedium,)
-          ],)
+              Text(
+                'Best Seller',
+                style: Styles.textStyle18,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              BestSellerListViewItem(),
+            ],
+          )
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.145,
+          child: AspectRatio(
+            aspectRatio: 3.5 / 5.5,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.yellow,
+                  image: const DecorationImage(
+                      image: AssetImage(AssetData.item), fit: BoxFit.fill)),
+            ),
+          ),
+        ),
+        const SizedBox(width: 30,),
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('''Harry Potter 
+and the Goblet of Fire'''),
+            Text('J.K. Rowling'),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('19.99 \$'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Text('4.8'),
+                    Text('(2390)')
+                  ],
+                )
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
