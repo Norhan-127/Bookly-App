85c421ec3fe1d070c6a7a604c0be4fbf40f85ae1
Norhan-127 <norhanhamza127@gmail.com>
2023-12-20 10:44:25 +0200

    search result view ui done

--- lib/features/search/presentaion /views/widgets/search_view_body.dart
+++ lib/features/search/presentaion /views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -6,14 +8,49 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: SafeArea(
-        child: Column(
-          children: [CustomSearchTextField()],
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 60,
+          ),
+          const CustomSearchTextField(),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Result',
+            style: Styles.textStyle20.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Expanded(child: SearchResultListView())
+        ],
       ),
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: BestSellerListViewItem(),
+        );
+      },
+      itemCount: 10,
+      padding: EdgeInsets.zero,
+    );
+  }
+}
