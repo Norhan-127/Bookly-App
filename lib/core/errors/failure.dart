70dcd019724448f243a6190dadf67710cd2dae5a
Norhan-127 <norhanhamza127@gmail.com>
2024-02-06 01:13:34 +0200

    Create Server failure from DioException

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,2 +1,45 @@
-abstract class Failure{}
-class ServerFailure implements Failure{}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+
+  factory ServerFailure.fromDioExeption(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with Server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout with Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with Server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate with Server');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromBadResponse(
+            dioException.response!.statusCode, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request Canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection Error');
+      case DioExceptionType.unknown:
+        return ServerFailure('unknown error');
+    }
+  }
+
+  factory ServerFailure.fromBadResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('your request not fount, please try again later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error, please try again later');
+    } else {
+      return ServerFailure('Opps there was an error, please try again later');
+    }
+  }
+}
