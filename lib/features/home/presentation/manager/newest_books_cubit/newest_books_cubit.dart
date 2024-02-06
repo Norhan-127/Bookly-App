8258f510554deb657b86e9beb9c17dcad3ed7e2d
Norhan-127 <norhanhamza127@gmail.com>
2024-02-06 18:29:21 +0200

    create newest books cubit method

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,6 +1,18 @@
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoadingState());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailureState(failure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSuccessState(books));
+    });
+  }
 }
