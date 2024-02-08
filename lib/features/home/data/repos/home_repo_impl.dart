7b9e39d4caeef79f08223090c6e322161b4d7e85
Norhan-127 <norhanhamza127@gmail.com>
2024-02-08 14:42:18 +0200

    fetch similar books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -59,4 +59,29 @@ class HomeRepoImpl implements HomeRepo {
 
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async {
+    try {
+      var data = await apiService.get(
+          endPoint: "?Filtering=free-ebooks&q=subject:Programming&Sorting=relevance");
+      List<BookModel> books =[ ];
+      for(var item in data["items"]){
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    }
+    catch(e) {
+      if(e is DioException){
+
+        return left(ServerFailure.fromDioExeption(e));
+
+      }
+      return left(ServerFailure(
+          e.toString()
+      ));
+
+    }
+  }
 }
